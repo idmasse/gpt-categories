1ba9d5894d234eff8af953d33eef55c7ed8f0aa8
Ian Masse <ianm@Ians-MBP.localdomain>
2024-08-29 11:12:05 -0700

    updates openaitest.py

--- openaitest.py
+++ openaitest.py
@@ -19,22 +19,19 @@ with open('additional_info.json', 'r') as h:
 def categorize_products(title, description, product_url):
     response = gptclient.chat.completions.create(
         model="gpt-4o",
-        temperature=0.3,
+        temperature=0.5,
         top_p=1,
         messages=[
             {
                 "role":"user",
                 "content":f"""
-                I have a strict list of categories and sub-categories. Categorize the following product. 
-                I also have a strict list of attributes, find any applicable attributes for the product.
-                I also have a strict list of additional info, use it to find any additional information about the product.
-                
-                Use the supplied product title, description, brand name and product URL to find the above requested information. Parse information from the website to retrieve data. 
-                
+                I have a strict list of categories and sub-categories. Use them to categorize the following product. 
+                I also have a strict list of attributes. Use them find any applicable attributes for the product.
+                I also have a strict list of additional info, use it to find any additional information about the product. 
+            
                 Title: {title}
                 Description: {description}
                 Product URL: {product_url}
-                Brand Name: {brand_name}
                 
                 Categories and sub-categories are:
                 {categories}
@@ -45,15 +42,15 @@ def categorize_products(title, description, product_url):
                 Additional Information:
                 {additional_info}
 
-                Only choose from the listed categories and sub-categories. Format response in JSON.
-
-                Only match found attributes to the attribute codes from attribute_data. Format response in JSON.
-
-                Additional information - Use your best judgment to reply with additional information. Format response in JSON.
-                
-                If you aren't confident what the categories should be, do not hallucinate them. Reply with "Categories:null"
-                If you aren't confident what the attributes should be, do not hallucinate them. Reply with "Attributes:null"
-                If you are not able to find additional information, do not include it.
+                Instructions:
+                - Use the supplied product title, description, and product URL to find the above requested information. 
+                - Parse information from the website to retrieve data.
+                - Only choose from the listed categories and sub-categories. Format the response as JSON.
+                - Only match found attributes to the attribute codes from attribute_data. Format the response as JSON.
+                - Use your best judgment to reply with additional information. Format the response as JSON.
+                - If you aren't confident about the categories, reply with `"Categories": null`.
+                - If you aren't confident about the attributes, reply with `"Attributes": null`.
+                - If you are not able to find additional information, do not include it in the response.
                 """
             }
         ]
@@ -61,10 +58,10 @@ def categorize_products(title, description, product_url):
     
     return response.choices[0].message.content.strip()
 
-brand_name = 'Oniva'
+
 title = "Reclining Camp Chair"
 description = """ Plush, portable, and packable, the Picnic Time Reclining Camp Chair is your camping cockpit with three reclining positions, an extra-wide, padded polyester seat and armrests, and much more. """
-product_url = "https://www.picnictime.com/products/reclining-camp-chair"
+product_url = "https://www.picnictime.com/"
 
 categorized = categorize_products(title, description, product_url)
 
