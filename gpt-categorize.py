1c9473f3e936a7e5674471a038f7be906b33e83d
Ian Masse <ianm@Ians-MBP.localdomain>
2024-09-12 09:33:57 -0700

    updates gpt-categorize.py

--- gpt-categorize.py
+++ gpt-categorize.py
@@ -1,19 +1,15 @@
 from openai import OpenAI
 import json
 import os
-import time
 from dotenv import load_dotenv
 
 load_dotenv()
 
-gptclient = OpenAI(api_key=os.getenv('API_KEY')) # initialize gpt client
+gptclient = OpenAI(api_key=os.getenv('OPENAI_API_KEY')) # initialize gpt client
 
-# load categories, attributes, additional info files
+# load categories and additional info files
 with open('categories.json', 'r') as f:
     categories = json.load(f)
-
-with open('attribute_data.json', 'r') as g:
-    attribute_data = json.load(g)
 
 with open('additional_info.json', 'r') as h:
     additional_info = json.load(h)
@@ -28,28 +24,20 @@ def categorize_products(product_info):
             {   
                 "role":"user",
                 "content":f"""
-                I have a strict list of categories and sub-categories. Use them to categorize the following product. 
-                I also have a strict list of attributes. Use them find any applicable attributes for the product.
+                I have a strict list of categories and sub-categories. Use them to categorize the products.
                 I also have a strict list of additional info, use it to find any additional information about the product. 
                 
                 Categories and sub-categories are:
                 {categories}
 
-                Attribute codes are:
-                {attribute_data}
-
                 Additional Information:
                 {additional_info}
 
                 Instructions:
+                - Format response as a list of dicts in JSON. Do not include ```JSON in the response.
                 - These are the products you are categorizing: {product_info}
-                - Use the supplied product title, description, and product URL https://refinerynumberone.com/.
-                - Only choose from the listed categories and sub-categories. Format the response as JSON.
-                - Only match found attributes to the attribute codes from attribute_data. Format the response as JSON.
-                - Use your best judgment to reply with additional information. Format the response as JSON.
-                - If you aren't confident about the categories, reply with `"Categories": null`.
-                - If you aren't confident about the attributes, reply with `"Attributes": null`.
-                - If you are not able to find additional information, do not include it in the response.
+                - Only choose from the listed categories and sub-categories.
+                - Only choose from the listed additional info. 
                 """
             }
         ]
@@ -58,9 +46,9 @@ def categorize_products(product_info):
     return response.choices[0].message.content.strip()
 
 # specify the folder containing the product_info files
-input_folder_path = 'split_json_missing'
+input_folder_path = ''
 # specify the folder to save the results
-output_folder_path = 'refinery-gpt-results2'
+output_folder_path = ''
 
 # ensure the output folder exists
 os.makedirs(output_folder_path, exist_ok=True)
